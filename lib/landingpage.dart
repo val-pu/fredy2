9a23c4f84ba18678abf2638c9d16acbb3b3d6c8b
val-mx <val.pukhov@gmail.com>
2023-03-09 11:54:35 +0100

    Landingpage hinzugefuegt

--- lib/landingpage.dart
+++ lib/landingpage.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:fredy2/groups/grouppage.dart';
 
 class LandingPage extends StatefulWidget {
-  const LandingPage({super.key});
+  LandingPage({super.key}) {
+    groups.add(GroupItem("Test", "name", "description"));
+  }
+
+  final groups = <GroupItem>[GroupItem("id", "name", "description")];
 
   @override
   State<StatefulWidget> createState() {
@@ -10,11 +15,40 @@ class LandingPage extends StatefulWidget {
 }
 
 class _LandingPageState extends State<LandingPage> {
+  void startGroupActivity(String id) {
+    var s = const SnackBar(
+      content: Text("Landing Page"),
+    );
+    Navigator.push(context,
+        MaterialPageRoute(builder: (context) => GroupPage(groupId: id)));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-      title: const Text("a"),
-    ));
+          title: const Text("a"),
+        ),
+        body: ListView.builder(
+          itemCount: widget.groups.length,
+          itemBuilder: (context, i) {
+            var currentGroup = widget.groups[i];
+            return ListTile(
+              title: Text(currentGroup.name),
+              onTap: () {
+                startGroupActivity(currentGroup.id);
+              },
+              subtitle: Text(currentGroup.description),
+            );
+          },
+        ));
   }
+}
+
+class GroupItem {
+  final String id;
+  final String name;
+  final String description;
+
+  GroupItem(this.id, this.name, this.description);
 }
