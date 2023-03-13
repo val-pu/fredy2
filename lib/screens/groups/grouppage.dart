115c9a752bd8fba02b81aacf5577179c5a569166
val-mx <val.pukhov@gmail.com>
2023-03-13 14:55:44 +0100

    Grouppage calendar UI added

--- lib/screens/groups/grouppage.dart
+++ lib/screens/groups/grouppage.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:fredy2/widgets/title.dart';
 
 class GroupPage extends StatefulWidget {
   GroupPage({super.key, required this.groupId});
@@ -19,67 +20,59 @@ class _GroupPageState extends State<GroupPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      appBar: AppBar(
-        title: Text(widget.groupName),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Text(
-            widget.groupDescription,
-            style: const TextStyle(fontWeight: FontWeight.bold),
-          ),
-          Padding(
-              padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 80),
-              child: DecoratedBox(
-                decoration: BoxDecoration(
-                    color: Theme.of(context).colorScheme.primary,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.5),
-                        spreadRadius: 1,
-                        blurRadius: 7,
-                        offset: const Offset(0, 2),
-                      ),
-                    ],
-                    borderRadius: const BorderRadius.all(Radius.circular(10))),
-                child: Padding(
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        Text("Sehe alle Vorschlaege ein"),
-                        Icon(Icons.chevron_right)
-                      ]),
-                ),
-              )),
-          ListView.builder(
-              itemCount: 10,
-              itemBuilder: (context, i) {
-                return Container(
-                    color: Colors.amberAccent,
-                    child: Column(
-                      children: [
-                        Row(
-                          mainAxisSize: MainAxisSize.max,
-                          children: [
-                            Container(
-                              color: Colors.amber,
-                              child: Text("Eventname"),
-                            )
-                          ],
-                        ),
-                        const Padding(
-                          padding: EdgeInsets.all(20.0),
-                          child: Text("MO"),
-                        )
-                      ],
-                    ));
-              })
-        ],
-      ),
-    );
+        backgroundColor: Theme.of(context).colorScheme.background,
+        appBar: AppBar(
+          title: Text(widget.groupName),
+        ),
+        body: Column(
+          children: [
+            Row(
+              children: [
+                FredTitle(
+                  text: "Aktuelle Veranstaltunegn",
+                  icon: Icons.calendar_today_outlined,
+                )
+              ],
+            ),
+            Expanded(
+              child: ListView.builder(
+                  itemCount: 10,
+                  physics: const NeverScrollableScrollPhysics(),
+                  primary: true,
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, i) {
+                    return Container(
+                        margin: const EdgeInsets.all(10),
+                        child: Wrap(children: [
+                          DecoratedBox(
+                              decoration: const BoxDecoration(
+                                  color: Colors.blue,
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(4))),
+                              child: Column(
+                                children: [
+                                  Row(
+                                    mainAxisSize: MainAxisSize.max,
+                                    children: [
+                                      Container(
+                                        color: Colors.indigo,
+                                        child: const Padding(
+                                            padding: EdgeInsets.all(8.0),
+                                            child: Text("Eventname")),
+                                      )
+                                    ],
+                                  ),
+                                  const Padding(
+                                    padding: EdgeInsets.all(15.0),
+                                    child: Text("MO"),
+                                  )
+                                ],
+                              ))
+                        ]));
+                  }),
+            )
+          ],
+        ));
   }
 }
